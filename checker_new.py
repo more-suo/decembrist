44f83145c1fd5e22e3c3d5ecdda28e8dd2bba2a5
more-suo <60295052+more-suo@users.noreply.github.com>
2020-09-03 13:27:05 +0500

    Delete checker_new.py

--- checker_new.py
+++
@@ -1,9 +0,0 @@
-import functions_for_amina as f
-import resource
-
-resource.setrlimit(resource.RLIMIT_AS, (2**28, 2**28))
-resource.setrlimit(resource.RLIMIT_FSIZE, (2**20, 2**20))
-
-program_name = 'program1'
-print(f.check(program_name, "c"))
-
