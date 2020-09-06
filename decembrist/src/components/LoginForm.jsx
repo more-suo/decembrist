b7cd402eaa093b120916e17a412dfb553db08e8d
thmbele <th.mbele@gmail.com>
2020-09-07 02:11:16 +0500

    Split index.js to LoginForm.jsx

---
+++ decembrist/src/components/LoginForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function LoginForm() {
+    return (
+        <form className="box" name="main-form" id="main-form">
+            <div className="login-form">
+                <h2>Hi, I'm Julia.</h2>
+                <div className="form-info" id="form-info">Tell me your username and password.</div>
+                <label htmlFor="username"></label>
+                <input type="text" name="Username" id="username" placeholder="Username"></input>
+                <label htmlFor="password"></label>
+                <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"></input>
+                <button className="major-button" type="button" name="Login" value="Login"
+                        id="login-button">Login
+                </button>
+                <div className="unusual-actions">
+                    <div className="reset-password"><span id="change-to-reset-password-form">I forgot my password.</span>
+                    </div>
+                    <div className="unusual-actions-sep">||</div>
+                    <div className="registration"><span
+                        id="change-to-registration-form">We're not acquainted.</span></div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
--- decembrist/src/index.js
+++ decembrist/src/index.js
@@ -1,32 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import './index.css'
-
-
-function LoginForm() {
-    return (
-        <form className="box" name="main-form" id="main-form">
-            <div className="login-form">
-                <h2>Hi, I'm Julia.</h2>
-                <div className="form-info" id="form-info">Tell me your username and password.</div>
-                <label htmlFor="username"></label>
-                <input type="text" name="Username" id="username" placeholder="Username"></input>
-                <label htmlFor="password"></label>
-                <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"></input>
-                    <button className="major-button" type="button" name="Login" value="Login"
-                            id="login-button">Login
-                    </button>
-                    <div className="unusual-actions">
-                        <div className="reset-password"><span id="change-to-reset-password-form">I forgot my password.</span>
-                        </div>
-                        <div className="unusual-actions-sep">||</div>
-                        <div className="registration"><span
-                            id="change-to-registration-form">We're not acquainted.</span></div>
-                    </div>
-            </div>
-        </form>
-    )
-}
-
+import "./index.css"
+import App from "./components/App";
 
 render(<App/>, document.getElementById('App'))
