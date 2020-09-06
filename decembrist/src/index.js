b506c21b9882ca47866d32943fc989d6eacd00a2
thmbele <th.mbele@gmail.com>
2020-09-07 00:37:01 +0500

    Add font.css

--- decembrist/src/index.js
+++ decembrist/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Article() {
+    const body = <section>body</section>
+    return (
+        <div className="hello" style={{color: 'red'}}>
+            <h2>title</h2>
+            {body}
+            <h3>creation date: {(new Date()).toDateString()}</h3>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    return (
+        <div>
+            <h1>App name</h1>
+            <Article />
+        </div>
+    )
+}
+
+render(<App/>, document.getElementById('root'))
