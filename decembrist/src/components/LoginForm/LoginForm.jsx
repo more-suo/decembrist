55136fcc83cf60ab9d17632ba76495ab353a540a
thmbele <th.mbele@gmail.com>
2020-09-07 17:16:19 +0500

    Add styles for LoginForm

--- decembrist/src/components/LoginForm/LoginForm.jsx
+++ decembrist/src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './LoginForm.css';
 
 function LoginForm() {
     return (
@@ -10,15 +11,16 @@ function LoginForm() {
                 <input type="text" name="Username" id="username" placeholder="Username"/>
                 <label htmlFor="password"/>
                 <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"/>
-                <button className="major-button" type="button" name="Login" value="Login"
-                        id="login-button">Login
+                <button className="major-button" type="button" name="Login" value="Login" id="login-button">
+                    Login
                 </button>
                 <div className="unusual-actions">
-                    <div className="reset-password"><span id="change-to-reset-password-form">I forgot my password.</span>
+                    <div className="reset-password">
+                        <span id="change-to-reset-password-form">I forgot my password.</span>
                     </div>
-                    <div className="unusual-actions-sep">||</div>
-                    <div className="registration"><span
-                        id="change-to-registration-form">We're not acquainted.</span></div>
+                    <div className="registration">
+                        <span id="change-to-registration-form">We're not acquainted.</span>
+                    </div>
                 </div>
             </div>
         </form>
