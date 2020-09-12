51630fd250f0dc3972a65a96e48d3298cf1fead9
thmbele <th.mbele@gmail.com>
2020-09-13 04:56:30 +0500

    Remove irrelevant tags from LoginForm

--- decembrist/src/components/LoginForm/LoginForm.jsx
+++ decembrist/src/components/LoginForm/LoginForm.jsx
@@ -5,28 +5,26 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <div className="box" id="main-form">
-                <div className="login-form">
-                    <h2>Hi, I'm Julia.</h2>
-                    <div className="form-info" id="form-info">Tell me your username and password.</div>
-                    <label htmlFor="username"/>
-                    <input type="text" name="Username" id="username" placeholder="Username"/>
-                    <label htmlFor="password"/>
-                    <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"/>
-                    <button className="login-button" type="button" id="login-button">
-                        Login
-                    </button>
-                    <div className="change-form">
-                        <div className="reset-password">
-                             <span onClick={this.props.setForm.bind(this, "reset")} id="change-to-reset-password-form">
-                                 I forgot my password.
-                             </span>
-                        </div>
-                        <div className="registration">
-                             <span onClick={this.props.setForm.bind(this, "registration")} id="change-to-registration-form">
-                                 We're not acquainted.
-                             </span>
-                        </div>
+            <div className="login-form">
+                <h2>Hi, I'm Julia.</h2>
+                <div className="form-info" id="form-info">Tell me your username and password.</div>
+                <label htmlFor="username"/>
+                <input type="text" name="Username" id="username" placeholder="Username"/>
+                <label htmlFor="password"/>
+                <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"/>
+                <button className="login-button" type="button" id="login-button">
+                    Login
+                </button>
+                <div className="change-form">
+                    <div className="reset-password">
+                         <span onClick={this.props.setForm.bind(this, "reset")} id="change-to-reset-password-form">
+                             I forgot my password.
+                         </span>
+                    </div>
+                    <div className="registration">
+                         <span onClick={this.props.setForm.bind(this, "registration")} id="change-to-registration-form">
+                             We're not acquainted.
+                         </span>
                     </div>
                 </div>
             </div>
