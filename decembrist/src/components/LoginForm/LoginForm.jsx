7d4ab7efd6248e86c579316007a4c8cb06121a6e
thmbele <th.mbele@gmail.com>
2020-09-15 11:41:49 +0500

    Add ability to track login and password input

--- decembrist/src/components/LoginForm/LoginForm.jsx
+++ decembrist/src/components/LoginForm/LoginForm.jsx
@@ -2,6 +2,23 @@ import React, {Component} from "react";
 import './LoginForm.css';
 
 class LoginForm extends Component {
+    state = {
+        username: "",
+        password: "",
+    }
+
+    setUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    setPassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     handleLogin = () => {
         this.props.setApp("contest")
     }
@@ -12,9 +29,16 @@ class LoginForm extends Component {
                 <h2>Hi, I'm Julia.</h2>
                 <div className="form-info" id="form-info">Tell me your username and password.</div>
                 <label htmlFor="username"/>
-                <input type="text" name="Username" id="username" placeholder="Username"/>
+                <input type="text"
+                       placeholder="Username"
+                       value={this.state.username}
+                       onChange={this.setUsername}/>
                 <label htmlFor="password"/>
-                <input type="password" name="Password" id="password" placeholder="Password" autoComplete="on"/>
+                <input type="password"
+                       placeholder="Password"
+                       value={this.state.password}
+                       onChange={this.setPassword}
+                       autoComplete="on"/>
                 <button className="login-button" type="button" id="login-button" onClick={this.handleLogin}>
                     Login
                 </button>
