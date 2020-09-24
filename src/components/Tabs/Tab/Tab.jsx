c591bf62342cbcc6ab21976b30c2e1733ceef590
thmbele <th.mbele@gmail.com>
2020-09-24 20:27:00 +0500

    Add component Tab

--- src/components/Tabs/Tab/Tab.jsx
+++ src/components/Tabs/Tab/Tab.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class TabContent extends Component {
+class Tab extends Component {
     state = {
 
     }
@@ -12,4 +12,4 @@ class TabContent extends Component {
     }
 }
 
-export default TabContent
+export default Tab
