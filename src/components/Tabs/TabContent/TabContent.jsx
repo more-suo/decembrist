a4a4aee2d366eb75508dbff3479b391093fd5c60
thmbele <th.mbele@gmail.com>
2020-09-24 20:25:02 +0500

    Add component TabContent

---
+++ src/components/Tabs/TabContent/TabContent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class TabContent extends Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <div>This is the tab content</div>
+        )
+    }
+}
