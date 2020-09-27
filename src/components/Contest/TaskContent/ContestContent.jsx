4283fb6ac948cdcb63bf83c4673dd62798aefaf8
thmbele <th.mbele@gmail.com>
2020-09-27 21:12:32 +0500

    Change the way of creating tabs

--- src/components/Contest/TaskContent/ContestContent.jsx
+++ src/components/Contest/TaskContent/ContestContent.jsx
@@ -1,11 +1,33 @@
 import React, {Component} from "react";
 import TabContent from "../../Tabs/TabContent/TabContent";
 import "./ContestContent.css";
+import LimitsTable from "./LimitsTable/LimitsTable";
 
 class ContestContent extends Component {
+    state = {
+        id: 0,
+        title: "Lorem Ipsum",
+        content: "Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task.",
+        contest: 0,
+        tl: 0,
+        ml: 0,
+        samples: [
+            ["123", "Metallica"],
+            ["123", "Alcohollica"]
+        ]
+    }
+
     render() {
         return(
-            <TabContent />
+            <div className="ContestContent">
+                <TabContent title={this.state.title}
+                            tl={this.state.tl}
+                            ml={this.state.ml}
+                            content={this.state.content}
+                            samples={this.state.samples}
+                />
+
+            </div>
         )
     }
 }
--- src/components/Tabs/TabContent/TabContent.jsx
+++ src/components/Tabs/TabContent/TabContent.jsx
@@ -3,26 +3,13 @@ import LimitsTable from "../../Contest/TaskContent/LimitsTable/LimitsTable";
 import SamplesTable from "../../Contest/TaskContent/SamplesTable/SamplesTable";
 
 class TabContent extends Component {
-    state = {
-        id: 0,
-        title: "Lorem Ipsum",
-        content: "Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task.",
-        contest: 0,
-        tl: 0,
-        ml: 0,
-        samples: [
-            ["123", "Metallica"],
-            ["123", "Alcohollica"]
-        ]
-    }
-
     render() {
         return (
             <div>
-                <h1 className="title">{this.state.title}</h1>
-                <LimitsTable tl={this.state.tl} ml={this.state.ml}/>
-                <div className="content">{this.state.content}</div>
-                <SamplesTable samples={this.state.samples}/>
+                <h1 className="title">{this.props.title}</h1>
+                <LimitsTable tl={this.props.tl} ml={this.props.ml}/>
+                <div className="content">{this.props.content}</div>
+                <SamplesTable samples={this.props.samples}/>
             </div>
         )
     }
