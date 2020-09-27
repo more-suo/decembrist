92cd1cab42421bcf2ee2498ad138700393fb6ec9
thmbele <th.mbele@gmail.com>
2020-09-27 21:19:18 +0500

    Create way to save tasks in array

--- src/components/Contest/TaskContent/ContestContent.jsx
+++ src/components/Contest/TaskContent/ContestContent.jsx
@@ -17,6 +17,34 @@ class ContestContent extends Component {
         ]
     }
 
+    task0 = {
+        id: 0,
+        title: "Lorem Ipsum",
+        content: "Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task.",
+        contest: 0,
+        tl: 0,
+        ml: 0,
+        samples: [
+            ["123", "Metallica"],
+            ["123", "Alcohollica"]
+        ]
+    }
+
+    task1 = {
+        id: 1,
+        title: "Metallica And Bread",
+        content: "Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too!",
+        contest: 1,
+        tl: 666,
+        ml: 666,
+        samples: [
+            ["666", "'tallica"],
+            ["123", "Alcohollica"]
+        ]
+    }
+
+    tasks = [this.task0, this.task1]
+
     render() {
         return(
             <div className="ContestContent">
