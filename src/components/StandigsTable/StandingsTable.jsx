542e749afe3670850fd6bc55fd2bc8af91c23b9c
thmbele <th.mbele@gmail.com>
2020-10-04 03:40:16 +0500

    Add component StandingsTable

---
+++ src/components/StandigsTable/StandingsTable.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+class StandingsTable extends Component {
+    render() {
+        return (
+            <div className="StandingsTable">
+
+            </div>
+        )
+    }
+}
+
+export default StandingsTable
