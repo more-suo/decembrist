43e0581b172341da1cbeddc2df772fcaf896a73f
thmbele <th.mbele@gmail.com>
2020-10-06 03:29:32 +0500

    Add basic table to StandingsTable

--- src/components/StandigsTable/StandingsTable.jsx
+++ src/components/StandigsTable/StandingsTable.jsx
@@ -5,7 +5,16 @@ class StandingsTable extends Component {
     render() {
         return (
             <div className="StandingsTable">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Task I</th>
+                    </tr>
+                </thead>
+                <tbody>
 
+                </tbody>
             </div>
         )
     }
