32faae37a5d20d645ee16edacba420c1e1784d7f
thmbele <th.mbele@gmail.com>
2020-10-13 00:09:35 +0500

    Add expression for requests error

--- src/components/Contest/Contest.jsx
+++ src/components/Contest/Contest.jsx
@@ -4,15 +4,17 @@ import ContestContent from "../TaskContent/ContestContent";
 
 class Contest extends Component {
 
+
     componentDidMount() {
         fetch("https://julia-api.azurewebsites.net/api/tasks")
             .then(res => res.json())
             .then(
                 (result) => {
-                    this.setState({
-
-                    })
+                    console.log(result.count);
                 }
+            )
+            .catch(
+                console.log("Error")
             )
     }
 
