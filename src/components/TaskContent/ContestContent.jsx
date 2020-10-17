1da766e93afcd11055663fa6bb313f7cd495a479
thmbele <th.mbele@gmail.com>
2020-10-18 04:23:56 +0500

    Refactor code

--- src/components/TaskContent/ContestContent.jsx
+++ src/components/TaskContent/ContestContent.jsx
@@ -14,27 +14,25 @@ class ContestContent extends Component {
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
-        // if (nextProps.tasks !== this.state.props.tasks){
-            console.log(this.props.tasks);
-            const apiUrl = "https://julia-api-server.herokuapp.com/api/";
-            nextProps.tasks.forEach( element => {
-                fetch(apiUrl + "tasks/" + element.toString(), {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                    }
-                })
-                    .then(res => res.json())
-                    .then(
-                        (result) => {
-                            this.state.tasks.push(result)
-                        }
-                    )
-                    .catch(
-                        console.log
-                    )
+        console.log(this.props.tasks);
+        const apiUrl = "https://julia-api-server.herokuapp.com/api/";
+        nextProps.tasks.forEach( element => {
+            fetch(apiUrl + "tasks/" + element.toString(), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
             })
-        // }
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.state.tasks.push(result)
+                    }
+                )
+                .catch(
+                    console.log
+                )
+        })
     }
 
     toggleSidebar = () => {
@@ -72,34 +70,6 @@ class ContestContent extends Component {
 
 
     render() {
-        // let task0 = {
-        //     id: 0,
-        //     title: "Lorem Ipsum",
-        //     content: "Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task. Something 'bout the task.",
-        //     contest: 0,
-        //     tl: 0,
-        //     ml: 0,
-        //     samples: [
-        //         ["123", "Metallica"],
-        //         ["123", "Alcohollica"]
-        //     ]
-        // }
-        // let task1 = {
-        //     id: 1,
-        //     title: "Metallica And Bread",
-        //     content: "Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too! Metallica likes beer. They have great beards and hair. Slayer is cool too!",
-        //     contest: 1,
-        //     tl: 666,
-        //     ml: 666,
-        //     samples: [
-        //         ["666", "'tallica"],
-        //         ["123", "Alcohollica"]
-        //     ]
-        // }
-
-
-        // let tasks = this.state.tasks
-
         let tabs = {}
         let tabTitles = {}
         this.state.tasks.forEach(element => {
@@ -114,7 +84,7 @@ class ContestContent extends Component {
             tabTitles[element.id] = element.title
         });
 
-        return(
+        return (
             <div>
                 <Sidebar isToggled={this.state.sidebarIsToggled}
                          tabTitles={tabTitles}
