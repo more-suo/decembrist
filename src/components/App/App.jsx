648fd703524cc073e3ce49fd507c6dcaec3f7264
arcturus5340 <arcturus5340@gmail.com>
2021-03-26 15:45:57 +0100

    Integrate the api library into the authorization form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
-import AuthForm from "../AuthForm/AuthForm";
-import Contest from "../Contest/Contest";
+// import Contest from "../Contest/Contest";
 import MainPage from "../MainPage/MainPage";
 import "./App.css";
-
+import LoginForm from "../LoginForm/LoginForm";
+import {JuliaAPI} from "julia-api"
 
 class App extends Component {
-    state = {
-        app: "auth",
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: "login",
+        }
     }
 
     setApp = (AppName) => {
@@ -17,18 +20,20 @@ class App extends Component {
     }
 
     render() {
+        let api = new JuliaAPI('localhost:8000', false);
         let window = [];
-        if (this.state.app === "auth"){
-            window.push(<AuthForm setApp={this.setApp}/>)
-        } else if (this.state.app === "contest"){
-            window.push(<Contest setApp={this.setApp}/>)
-        } else if (this.state.app === "MainPage"){
-            window.push(<MainPage setApp={this.setApp}/>)
-        } else {
-            window.push(<h1 className="errorMessage">What are you trying to do? Error 404! :|</h1>)
+
+        if (this.state.view === "login"){
+            window.push(<LoginForm api={api}/>);
+        {/*} else if (this.state.app === "contest"){*/}
+        {/*    window.push(<Contest setApp={this.setApp}/>)*/}
+        }else if (this.state.view === "main") {
+            window.push(<MainPage setApp={this.setApp}/>);
+        }else {
+            window.push(<h1 className="errorMessage">What are you trying to do? Error 404! :|</h1>);
         }
 
-        return window
+        return window;
     }
 }
 
---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,115 @@
+import React, {Component} from "react";
+import './LoginForm.css';
+// import Cookies from 'universal-cookie';
+
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            info_message: "Tell me your username and password."
+        }
+    }
+
+    setUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    setPassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleLogin = () => {
+        if (this.state.username === ""){
+            this.setState({
+                info_message: "I need your username, tell me!"
+            })
+            return false;
+        } else if (this.state.password === "") {
+            this.setState({
+                info_message: "I need your password too!"
+            })
+            return false;
+        }
+
+        this.login();
+    }
+
+    login = () => {
+        this.props.api.obtainToken(this.state.username, this.state.password).then(
+            (response) => {
+                switch (response.status){
+                    case 200:
+                        this.refreshToken(response.data.token);
+                        break;
+                    case 400:
+                        // TODO: Wrong username or password
+                        console.log(response.data);
+                        break;
+                    default:
+                        // TODO: handle unexpected responses
+                }
+            }
+
+
+        )
+    }
+
+    refreshToken = (token) => {
+        this.props.api.refreshToken(token).then(
+            (response) => {
+                console.log(response.data);
+                switch (response.status){
+                    case 200:
+                        this.props.api.setAuthToken(response.data.token);
+                        break;
+                    default:
+                        // TODO: handle unexpected responses
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="login-form">
+                <h2>Hi, I'm Julia.</h2>
+                <div className="form-info" id="form-info">{this.state.info_message}</div>
+                <label htmlFor="username"/>
+                <input type="text"
+                       placeholder="Username"
+                       value={this.state.username}
+                       onChange={this.setUsername}/>
+                <label htmlFor="password"/>
+                <input type="password"
+                       placeholder="Password"
+                       value={this.state.password}
+                       onChange={this.setPassword}
+                       autoComplete="on"/>
+                <button className="login-button" type="button" id="login-button" onClick={this.handleLogin}>
+                    Login
+                </button>
+                <div className="change-form">
+                    <div className="reset-password">
+                         {/*<span onClick={this.props.setForm.bind(this, "reset")} id="change-to-reset-password-form">*/}
+                         {/*    I forgot my password.*/}
+                         {/*</span>*/}
+                    </div>
+                    <div className="registration">
+                         {/*<span onClick={this.props.setForm.bind(this, "registration")} id="change-to-registration-form">*/}
+                         {/*    We're not acquainted.*/}
+                         {/*</span>*/}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LoginForm
